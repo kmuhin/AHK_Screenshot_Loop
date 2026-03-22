e08166ae959ce9f4d05ebfb82cb51b6c6ab7e9f3
Kat <57116477+kmuhin@users.noreply.github.com>
2026-03-22 22:49:19 +0300

    First commit

---
+++ TakeScreenshotLoop.ahk
@@ -0,0 +1,130 @@
+﻿; Автоматические скриншоты до двух мониторов.
+#Persistent
+
+; https://doggy8088.github.io/AutoHotkeyDocs/docs/misc/DPIScaling.htm
+; To enable per-monitor DPI awareness, call the following function prior to using functions that are normally affected by DPI scaling:
+DllCall("SetThreadDpiAwarenessContext", "ptr", -3, "ptr")
+
+; Корневая директория скриншотов
+DIR_DST := "sshots\"
+; период скриншотов в миллисекундах
+LOOP_TIMEOUT := 10000
+; тип изображение (расширение файла скриншота)
+; может быть .jpg, .png, .bmp и т.д.
+IMAGE_EXT := ".jpg"
+
+Loop {
+; Кол-во мониторов
+SysGet MonCount, MonitorCount
+; Параметры монитора 1
+SysGet Mon1, Monitor, 1
+Mon1W := Mon1Right + Mon1Left
+
+if (MonCount = 2) { 
+    ; Параметры монитора 2
+    SysGet Mon2, Monitor, 2
+    Mon2W := Mon2Right + Mon2Left
+    FullWidth := ABS(Mon1W - Mon2W)
+    FullHeight := Max(Mon1Bottom,Mon2Bottom)
+	Left := Min(Mon1Left, Mon2Left)
+} else {
+    FullWidth := Mon1Right
+    FullHeight := Mon1Bottom
+	Left := 0
+}
+    ; Поддиректория с датой
+    dname := DIR_DST . A_YYYY . "-" . A_MM . "-" . A_DD
+    FileCreateDir, %dname%
+    ; имя файла "shot 2025-12-18 130506.jpg"
+	fname:=( dname "\shot " A_YYYY "-" A_MM "-" A_DD " " A_Hour A_Min A_Sec IMAGE_EXT)
+	; MsgBox, %fname%
+	SaveScreenshotToFile(Left, 0, FullWidth, FullHeight, fname)
+    Sleep, %LOOP_TIMEOUT%
+}
+
+SaveScreenshotToFile(x, y, w, h, filePath)
+{
+	hBitmap := GetHBitmapFromScreen(x, y, w, h)
+	gdip := new GDIplus
+	pBitmap := gdip.BitmapFromHBitmap(hBitmap)
+	DllCall("DeleteObject", Ptr, hBitmap)
+	gdip.SaveBitmapToFile(pBitmap, filePath)
+	gdip.DisposeImage(pBitmap)
+}
+
+GetHBitmapFromScreen(x, y, w, h)
+{
+	hDC := DllCall("GetDC", Ptr, 0, Ptr)
+	hBM := DllCall("CreateCompatibleBitmap", Ptr, hDC, Int, w, Int, h, Ptr)
+	pDC := DllCall("CreateCompatibleDC", Ptr, hDC, Ptr)
+	oBM := DllCall("SelectObject", Ptr, pDC, Ptr, hBM, Ptr)
+	DllCall("BitBlt", Ptr, pDC, Int, 0, Int, 0, Int, w, Int, h, Ptr, hDC, Int, x, Int, y, UInt, 0x00CC0020)
+	DllCall("SelectObject", Ptr, pDC, Ptr, oBM)
+	DllCall("DeleteDC", Ptr, pDC)
+	DllCall("ReleaseDC", Ptr, 0, Ptr, hDC)
+	Return hBM
+}
+
+class GDIplus   {
+	__New()  {
+		if !DllCall("GetModuleHandle", Str, "gdiplus", Ptr)
+			DllCall("LoadLibrary", Str, "gdiplus")
+		VarSetCapacity(si, A_PtrSize = 8 ? 24 : 16, 0), si := Chr(1)
+		DllCall("gdiplus\GdiplusStartup", PtrP, pToken, Ptr, &si, Ptr, 0)
+		this.token := pToken
+	}
+	__Delete()  {
+		DllCall("gdiplus\GdiplusShutdown", Ptr, this.token)
+		if hModule := DllCall("GetModuleHandle", Str, "gdiplus", Ptr)
+			DllCall("FreeLibrary", Ptr, hModule)
+	}
+	BitmapFromHBitmap(hBitmap, Palette := 0)  {
+		DllCall("gdiplus\GdipCreateBitmapFromHBITMAP", Ptr, hBitmap, Ptr, Palette, PtrP, pBitmap)
+		return pBitmap
+	}
+	SaveBitmapToFile(pBitmap, sOutput, Quality=75)  {
+		SplitPath, sOutput,,, Extension
+		if Extension not in BMP,DIB,RLE,JPG,JPEG,JPE,JFIF,GIF,TIF,TIFF,PNG
+			return -1
+		DllCall("gdiplus\GdipGetImageEncodersSize", UIntP, nCount, UIntP, nSize)
+		VarSetCapacity(ci, nSize)
+		DllCall("gdiplus\GdipGetImageEncoders", UInt, nCount, UInt, nSize, Ptr, &ci)
+		if !(nCount && nSize)
+			return -2
+		Loop, % nCount  {
+			sString := StrGet(NumGet(ci, (idx := (48+7*A_PtrSize)*(A_Index-1))+32+3*A_PtrSize), "UTF-16")
+			if !InStr(sString, "*." Extension)
+				continue
+			pCodec := &ci+idx
+			break
+		}
+		if !pCodec
+			return -3
+		if RegExMatch(Extension, "i)^J(PG|PEG|PE|FIF)$") && Quality != 75  {
+			DllCall("gdiplus\GdipGetEncoderParameterListSize", Ptr, pBitmap, Ptr, pCodec, UintP, nSize)
+			VarSetCapacity(EncoderParameters, nSize, 0)
+			DllCall("gdiplus\GdipGetEncoderParameterList", Ptr, pBitmap, Ptr, pCodec, UInt, nSize, Ptr, &EncoderParameters)
+			Loop, % NumGet(EncoderParameters, "UInt")  {
+				elem := (24+A_PtrSize)*(A_Index-1) + 4 + (pad := A_PtrSize = 8 ? 4 : 0)
+				if (NumGet(EncoderParameters, elem+16, "UInt") = 1) && (NumGet(EncoderParameters, elem+20, "UInt") = 6)  {
+					p := elem+&EncoderParameters-pad-4
+					NumPut(Quality, NumGet(NumPut(4, NumPut(1, p+0)+20, "UInt")), "UInt")
+					break
+				}
+			}
+		}
+		if A_IsUnicode
+			pOutput := &sOutput
+		else  {
+			VarSetCapacity(wOutput, StrPut(sOutput, "UTF-16")*2, 0)
+			StrPut(sOutput, &wOutput, "UTF-16")
+			pOutput := &wOutput
+		}
+		E := DllCall("gdiplus\GdipSaveImageToFile", Ptr, pBitmap, Ptr, pOutput, Ptr, pCodec, UInt, p ? p : 0)
+		return E ? -5 : 0
+	}
+	DisposeImage(pBitmap)  {
+		return DllCall("gdiplus\GdipDisposeImage", Ptr, pBitmap)
+	}
+}
+Return
---
+++ make_video.py
@@ -0,0 +1,151 @@
+﻿# Прохожусь по подпапкам в директории `dir_shots`.
+# В каждой подпапке делаю из изображений видео с частотой одно изображения в секунду.
+# Формат mkv.
+# Требуется установленный ffmpeg.
+# Добавляю в субтитры названия файлов.
+# После создания субтитров с оригинальнымы названиями, переименовываю файлы изображений в 4-х значные номера по порядку: 0000.jpg, 0001.jpg, 0002.jpg, ...
+# После создания видео переименовываю изображения обратно в оригинальные имена.
+
+import os
+import subprocess
+import json
+from pathlib import Path
+from datetime import datetime, timedelta
+
+# временный файл субтитров
+file_srt = '!output.srt'
+# файл видео
+file_video = "!output.mkv"
+# маска изображений для видео
+files_mask = '*.jpg'
+# временный файл переименовывания файлов изображений
+file_renaming_map = "!renaming_map.json"
+# папка с подпапками изображений. Если поставить '', то будет искать подпапки в текущий директории.
+dir_shots = 'sshots/'
+
+# команда ffmpeg конвертирование изображений в видеофайл
+# %04d.jpg - формат файлов изображений типа: 0000.jpg, 0001.jpg, 0002.jpg, ...
+ffmpeg_cmd = f'''ffmpeg -hide_banner -framerate 1 -i %04d.jpg -i !output.srt -max_interleave_delta 0 {file_video}'''
+# Пример установки принудительного размера видео и отключения масштабирования.
+# ffmpeg_cmd =  f'''ffmpeg -framerate 1 -i %04d.jpg -i !output.srt -max_interleave_delta 0 -vf "scale=5120:1600:force_original_aspect_ratio=decrease,pad=5120:1600:-1:-1,setsar=1" {file_video}'''
+
+
+def convert_ffmpeg():
+    """
+        Запускаю ffmpeg в текущей папке.
+        Конвертирую изображения в видеофайл
+    """
+    result = subprocess.run(ffmpeg_cmd)
+    return True if result.returncode == 0 else False
+
+
+def create_srt(lines: list):
+    """
+     create subtitle file 
+     one line - one second
+
+    """
+    with open(file_srt, 'w') as f:
+        for idx, line in enumerate(lines):
+            f.write(f'{idx + 1}\n')
+            timestamp1 = timedelta(seconds=idx)
+            f.write(f"{timestamp1},000 --> {timestamp1},999\n")
+            f.write(f"{line}\n\n")
+
+
+def rename_files():
+    """
+    Переименовываю файлы для ffmpeg 
+    в 4-х значные номера по порядку: 0000.jpg, 0001.jpg, 0002.jpg, ...
+    и возвращаю карту переименований
+    list of{
+         'src': file,
+         'dst': file_new
+         }
+
+    :return: list of dict
+    """
+    print('Rename files.')
+    renaming_map = []
+    for idx, file in enumerate(Path().glob(files_mask)):
+        file_new = f'{idx:04}{file.suffix}'
+        # every name of file will be used in subtitles
+        renaming_map.append(
+            {'src': file,
+             'dst': file_new
+             })
+        file.rename(file_new)
+    return renaming_map
+
+
+def save_renaming_map(renaming_map):
+    with open(file_renaming_map, 'w', encoding='utf-8') as f:
+        # ensure_ascii - не экранировать не аски символы (русские  символы)
+        json.dump([{'src': str(i['src']), 'dst': str(i['dst'])} for i in renaming_map], f, indent=2, ensure_ascii=False)
+
+
+def read_renaming_map():
+    try:
+        with open(file_renaming_map, 'r', encoding='utf-8') as f:
+            data_json = json.loads(f.read())
+    except IOError:
+        return None
+    else:
+        return data_json
+
+
+def rename_files_back(renaming_map):
+    """
+        По карте переименований переименовываю файлы обратно
+    """
+    print('Rename files back.')
+    for f in renaming_map:
+        Path(f['dst']).rename(f['src'])
+
+
+def check_renaming():
+    renaming_map = read_renaming_map()
+    if renaming_map and Path(renaming_map[0]['dst']).exists():
+        print('Files are renamed already!')
+        return renaming_map
+
+
+def convert_current_folder():
+    """
+    create video file in current directory
+    """
+    if Path(file_video).exists():
+        print(f"File '{file_video}' exists")
+        return
+        rewrite = input(f"File '{file_srt}' exists. Rewrite file? [y/N]: ")
+        if rewrite.lower() != 'y':
+            return
+    # сначала проверяю, если уже было переименование и не было переименования обратно из-за прерывания программы
+    # 
+    renaming_map = check_renaming() or rename_files()
+    print(f"Files {len(renaming_map)}")
+    # create subtitles by file names
+    if renaming_map:
+        create_srt([i['src'] for i in renaming_map])
+        save_renaming_map(renaming_map)
+    convert_ffmpeg()
+    rename_files_back(renaming_map)
+
+
+def convert_subfolders():
+    """
+        Конвертирование во всех подпапках,
+        кроме подпапки текущего дня.
+    """
+    workdir = Path(__file__).parent.absolute()
+    date_now = datetime.now().date().isoformat()
+    for d in Path(dir_shots).glob('*'):
+        dpath = workdir.joinpath(d)
+        print(dpath)
+        if date_now not in str(d) and dpath.is_dir():
+            os.chdir(dpath)
+            convert_current_folder()
+
+
+if __name__ == '__main__':
+    convert_subfolders()
